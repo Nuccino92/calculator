530cf87d383118bb0479de5c34b4ba789b37e476
Anthony Nucci <anthnucci@hotmail.com>
2021-08-08 00:43:08 -0400

    added event listeners for the numbers and operators

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
+
+let previousDisplay = document.querySelector('.previous-display');
+let currentDisplay = document.querySelector('.current-display');
+let number = document.querySelectorAll('.number');
+let operator = document.querySelectorAll('.operator');
+let clear = document.getElementById('clear');
+let plusMinus = document.getElementById('plus-minus');
+let back = document.getElementById('back');
+let float = document.getElementById('float');
+let equals = document.getElementById('equals');
+
+let previous = '';
+let current = '';
+let decimal = false;
+let operation = null;
+
 function add(a, b) {
     return a + b
 }
-
 function subtract(a, b) {
     return a - b
 }
-
 function multiply(a, b) {
     return a * b
 }
-
 function divide(a, b) {
     return a / b
 }
+
 
 function operate(operator, operand1, operand2) {
     if (operator == '+') {
@@ -28,3 +42,46 @@ function operate(operator, operand1, operand2) {
         return divide(operand1, operand2)
     }
 }
+
+number.forEach(number => {
+    number.addEventListener('click', (e) => {
+        if (e.target.innerText === '.' && !decimal){
+            decimal = true;
+        } else if (e.target.innerText === '.' && decimal){
+            return;
+        }
+        current += e.target.innerText;
+        currentDisplay.innerText = current;
+        operation = parseFloat(current);
+    })
+});
+
+operator.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+        if (!current) return;
+        decimal = false;
+        
+        current += e.target.innerText;
+        currentDisplay.innerText = current;
+
+        if (current.innerText = operator) {
+            moveText();
+            // calculate();
+        }
+    })
+})
+
+    clear.addEventListener('click', (e) => {
+        currentDisplay.innerText = '';
+        current = '';
+        previousDisplay.innerText = '';
+        previous = '';
+        operation = null;
+    })
+
+    function moveText() {
+        previous += current+ ' ';
+        previousDisplay.innerText = previous;
+        currentDisplay.innerText = '';
+        current = '';
+    }
